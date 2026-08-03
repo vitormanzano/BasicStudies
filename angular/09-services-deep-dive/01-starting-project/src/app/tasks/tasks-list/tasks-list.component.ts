0ba8a7043d3751db72e55576d99ca9959a616df2
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-08-03 16:11:35 -0300

    Change detection

---
+++ angular/09-services-deep-dive/01-starting-project/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, computed, inject, signal } from '@angular/core';
+
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
+import { DatePipe } from '@angular/common';
+import { TasksServiceToken } from '../../../main';
+import { TASK_STATUS_OPTION, TaskStatusOptionsProvider } from '../task.model';
+
+@Component({
+  selector: 'app-tasks-list',
+  standalone: true,
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css',
+  imports: [TaskItemComponent],
+  providers: [TaskStatusOptionsProvider]
+})
+export class TasksListComponent {
+  private tasksService = inject(TasksServiceToken);
+  private selectedFilter = signal<string>('all');
+  taskStatusOptions = inject(TASK_STATUS_OPTION);
+
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'all':
+        return this.tasksService.allTasks();
+      case 'open':
+        return this.tasksService
+          .allTasks()
+          .filter(task => task.status === 'OPEN');
+      case 'in-progress':
+        return this.tasksService
+          .allTasks()
+          .filter(task => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService
+          .allTasks()
+          .filter(task => task.status === 'DONE');
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
+
+  onChangeTasksFilter(filter: string) {
+    this.selectedFilter.set(filter);
+  }
+}
---
+++ angular/10-change-detection/01-starting-project/src/main.ts
@@ -0,0 +1,8 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideExperimentalZonelessChangeDetection()]
+}).catch((err) => console.error(err));
